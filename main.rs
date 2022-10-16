337df0db122fca528f3425c6cdb9641a6f7acee3
Zange10 <niklaszan@protonmail.com>
2022-10-16 03:26:38 +0200

    adding stint/stop struct and cli input

--- main.rs
+++ main.rs
@@ -1,4 +1,35 @@
 // Calculate possible strategies for races of the Trackmania Formula League TMFL
+
+use std::io;
+
+#[allow(dead_code)]
+struct Stop {
+    time: f64,
+    fuel: f64,
+}
+
+#[allow(dead_code)]
+struct Stint {
+    laps: i32,
+    init_fuel: f64,
+    end_fuel: f64,
+    tyre_cond: f64,
+}
+
+//const refuel_rate:f64 = 4.0;
+//const pitlane_time:f64 = 20.0;
+
+
+
 fn main() {
-    println!("Hello");
+    let race_laps = get_input_i32("Race Laps: ");
+    println!("{}", race_laps);
+}
+
+fn get_input_i32(msg:&str) -> i32 {
+    println!("{}", msg);
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let x:i32 = input_line.trim().parse().expect("Input not an integer");
+    return x;
 }
